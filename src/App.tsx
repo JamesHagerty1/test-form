39898c0f20361dcd5b7d5ed23176fda9f3d84bf2
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2023-07-25 22:13:14 -0400

    functionality, now need style

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,88 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
+type TableVals = { [key: string]: number };
 
 function App() {
+  const [tableVals, setTableVals] = useState<TableVals>({});
   const [doHypothesisTest, setDoHypothesisTest] = useState(false);
 
   const handleSubmit = (values: any, { setSubmitting }: any) => {
-    console.log(values.sampleSize);
+    const table: TableVals = {};
+    
+    table['sample size'] = values.sampleSize;
+    table['sample mean'] = values.sampleMean;
+    table['standard deviation'] = values.standardDeviation;
+
+    if (doHypothesisTest) {
+      table['hypothesized mean'] = values.hypothesizedMean;
+    }
+
+    setTableVals(table);
     setSubmitting(false);
+  };
+
+  interface FormItemProps {
+    name: string;
+    labelText: string;
+    disabled: boolean;
+  }
+
+  const FormItem: React.FC<FormItemProps> = ({ name, labelText, disabled }) => {
+    return (
+      <div>
+        <div>
+          <label htmlFor={name}>{labelText}</label>
+          <Field
+            className='border border-gray-400 rounded-sm' 
+            type='number'
+            name={name}
+            disabled={disabled}
+          />
+        </div>
+        <ErrorMessage
+          className='text-red-600' 
+          name={name}
+          component='div' 
+        />
+      </div>
+    );
   };
 
   return (
     <div>
       <Formik
-        initialValues={{ sampleSize: '', hypothesizedMean: '' }}
+        initialValues={{
+          sampleSize: '',
+          sampleMean: '',
+          standardDeviation: '', 
+          hypothesizedMean: '' 
+        }}
         validate={values => {
+          setTableVals({});
+
           const errors: any = {};
 
           if (
             !Number.isInteger(values.sampleSize) ||
             parseInt(values.sampleSize) < 2
           ) {
-            errors.sampleSize = 'Need a whole number >= 2'
+            errors.sampleSize = 'Need a whole number >= 2';
+          }
+
+          if (values.sampleMean === '') {
+            errors.sampleMean = 'Need a numeric value';
+          }
+
+          if (
+            values.standardDeviation === '' || 
+            parseFloat(values.standardDeviation) <= 0
+          ) {
+            errors.standardDeviation = 'Need a numeric value > 0';
+          }
+
+          if (doHypothesisTest && values.hypothesizedMean === '') {
+            errors.hypothesizedMean = 'Need a numeric value';
           }
 
           return errors;
@@ -30,39 +91,34 @@ function App() {
       >
       {({ isSubmitting }) => (
         <Form>
-          <div>
-            <label htmlFor='sampleSize'>Sample size:</label>
-            <Field
-              className='border border-gray-400 rounded-sm' 
-              type='number' 
-              name='sampleSize' 
-            />
-          </div>
-          <ErrorMessage
-            className='text-red-600' 
-            name='sampleSize' 
-            component='div' 
+          <FormItem 
+            name={'sampleSize'}
+            labelText={'Sample size:'} 
+            disabled={false}
           />
           
-          <input                       
+          <FormItem 
+            name={'sampleMean'}
+            labelText={'Sample mean:'} 
+            disabled={false}
+          />
+
+          <FormItem 
+            name={'standardDeviation'}
+            labelText={'Standard deviation:'} 
+            disabled={false}
+          />
+
+          <input                       // modularize this one too for generalization
             type='checkbox'
             checked={doHypothesisTest}
             onChange={(e) => setDoHypothesisTest(e.target.checked)}
           />
           <label>Perform hypothesis test</label>
-          <div>
-            <label htmlFor='hypothesizedMean'>Hypothesized mean:</label>
-            <Field
-              className='border border-gray-400 rounded-sm' 
-              type='number' 
-              name='hypothesizedMean' 
-              disabled={!doHypothesisTest}
-            />
-          </div>
-          <ErrorMessage
-            className='text-red-600' 
-            name='hypothesizedMean' 
-            component='div' 
+          <FormItem
+            name={'hypothesizedMean'}
+            labelText={'Hypothesized mean:'}
+            disabled={!doHypothesisTest}
           />
 
           <br></br>
@@ -76,15 +132,27 @@ function App() {
           <button
             className='w-24 border border-gray-300'
             type='reset'
+            onClick={ () => { setTableVals({}) } }
           >
             Reset
           </button>
         </Form>
-       )}
-     </Formik>
-     {
-
-     }
+      )}
+      </Formik>
+      {(Object.keys(tableVals).length > 0) && (
+        <table>
+          <tbody>
+            {Object.keys(tableVals).map((title: string) =>
+              <tr
+                key={title}
+              >
+                <td>{title}</td>
+                <td>{tableVals[title]}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
